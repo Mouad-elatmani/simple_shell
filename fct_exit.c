8eeb67d7afa8041b9f9fb3bab920a6cd5544ac48
Mouad-elatmani <96914263+Mouad-elatmani@users.noreply.github.com>
2023-08-24 14:29:36 +0100

    Update fct_exit.c

--- fct_exit.c
+++ fct_exit.c
@@ -45,7 +45,7 @@ int cu_exit(char *line, int *status, int count, char *prog_name, char *message)
 		else
 		{
 			*status = 2;
-			print_cu_error(prog_name, count, *status, message);
+			print_cu_err(prog_name, count, *status, message);
 			free(line_copy);
 		}
 	}
